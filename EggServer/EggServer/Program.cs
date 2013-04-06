3023d98f76ea513a3b64338a04ac6782b6a5bd6a
homocury <homocury@gmail.com>
2013-04-07 01:01:11 +0900

    기본 네트웤

--- EggServer/EggServer/Program.cs
+++ EggServer/EggServer/Program.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using EggServer.Network;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EggServer
@@ -10,6 +13,26 @@ namespace EggServer
     {
         static void Main(string[] args)
         {
+            using (var mutex = new Mutex (false, "eggmoney"))
+            {
+                if (!mutex.WaitOne(0))
+                {
+                    Console.WriteLine ("서버가 이미 실행중입니다.");
+                    return;
+                }
+
+                RunProgram(args);
+            }
+        }
+
+        static void RunProgram(string[] args)
+        {
+            SocketServer server = new SocketServer(100, 100);
+            server.Init();
+            server.Start(new IPEndPoint(IPAddress.Any, 4444));
+
+            Console.WriteLine("Press any key to terminate the server process....");
+            Console.ReadKey();
         }
     }
 }
